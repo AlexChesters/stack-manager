e050b6e63dc6128dffdc63fb2c8a3c971094a5b3
Alex Chesters <alex.chesters@bbc.co.uk>
2018-02-11 16:16:40 +0000

    Add thing

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,12 @@
+import json from 'rollup-plugin-json'
+
 export default {
   input: 'src/stacker.js',
   output: {
     file: 'index.js',
     format: 'cjs'
-  }
+  },
+  plugins: [
+    json()
+  ]
 }
--- src/stacker.js
+++ src/stacker.js
@@ -1,1 +1,7 @@
-console.log('yo!')
+const program = require('commander')
+
+const { name, version } = require('./package.json')
+
+program
+  .version(`${name} - ${version}`)
+  .parse(process.argv)
