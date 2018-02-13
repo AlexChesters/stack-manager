b6160947ee843115547e70ede4530d3c7405b150
Alex Chesters <alex.chesters@bbc.co.uk>
2018-02-13 17:25:23 +0000

    Fix trailing comma

--- src/commands/delete/index.js
+++ src/commands/delete/index.js
@@ -5,7 +5,7 @@ const cloudformation = new AWS.CloudFormation({ region: 'eu-west-1' })
 module.exports = ({ projectName, templatePath }) => {
   const segments = templatePath.split('infrastructure/stacks/')
   const params = {
-    StackName: `${projectName}--${segments[segments.length - 1].replace(/\//g, '-').slice(0, -5)}`,
+    StackName: `${projectName}--${segments[segments.length - 1].replace(/\//g, '-').slice(0, -5)}`
   }
   return cloudformation.deleteStack(params).promise()
 }
