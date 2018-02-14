35533515084e9722e56665eb14aad84800da80fa
Alex Chesters <alex.chesters@bbc.co.uk>
2018-02-14 14:34:53 +0000

    Fix bad docs

--- src/commands/delete/index.js
+++ src/commands/delete/index.js
@@ -9,9 +9,6 @@ const generateStackName = require('../../utils/generate-stack-name')
  * @function delete
  * @memberof stack-manager
  * @param {String} templatePath The path to your Cloudformation template.
- * @param {String} [capability] Capability to specify when creating your stack.
- * Defaults to 'CAPABILITY_IAM'. All capabilities can be found
- * [here]{@link https://docs.aws.amazon.com/AWSCloudFormation/latest/APIReference/API_CreateStack.html}.
  * @returns {Promise} Resolves when the stack enters the
  * [DELETE_COMPLETE]{@link https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/using-cfn-describing-stacks.html#w2ab2c15c15c17c11}
  * state.
